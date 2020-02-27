4f5d2b747c53c6e22c5db0a74c204a1aa9e85366
khw1031 <khw1031@gmail.com>
2020-02-28 03:37:33 +0900

    Apply apollo-server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,27 @@
 /* eslint-disable no-console */
+import { ApolloServer } from "apollo-server-express";
 import { app } from "./app";
-import baseUrl from "./config/base-url";
+import { baseUrl, origin } from "./config";
+import schema from "./middlewares/graphql/schema";
 
 app.set("port", process.env.PORT || 7000);
 app.set("env", process.env.NODE_ENV || "development");
 
+const server = new ApolloServer({
+  schema,
+});
+
+server.applyMiddleware({
+  app,
+  cors: {
+    credentials: true,
+    origin: origin[app.get("env")],
+  },
+});
+
 app.listen(app.get("port"), () => {
   console.log(`Learnch is running at ${baseUrl[app.get("env")]}`);
+  console.log(`GraphQL server is running at ${baseUrl[app.get("env")]}${server.graphqlPath}`);
   console.log(`port: ${app.get("port")}`);
   console.log(`mode: ${app.get("env")} mode`);
 });
