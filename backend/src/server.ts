db9d3007c86912c298a350d47d79ccaa04a50787
khw1031 <khw1031@gmail.com>
2020-02-27 20:06:04 +0900

    Ignore console lint on server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,17 +3,7 @@ import { app } from "./app";
 app.set("port", process.env.PORT || 7000);
 app.set("env", process.env.NODE_ENV || "development");
 
-app.get("/", (req, res) => {
-  res.json({
-    hello: "world",
-  });
-});
-
-app.post("/api/ping", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
-});
-
 app.listen(app.get("port"), () => {
+  /* eslint-disable-next-line no-console */
   console.log(`running at http://localhost:${app.get("port")}`);
 });
